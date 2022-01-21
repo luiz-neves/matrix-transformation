14b533467b4c5531d98667695fb97a6d9498758d
Isabelle Porto <isabellenevesporto@hotmail.com>
2022-01-20 23:23:04 -0300

    Add tests

--- src/test/kotlin/Test.kt
+++ src/test/kotlin/Test.kt
@@ -1,10 +1,55 @@
 import io.kotest.core.spec.style.FreeSpec
+import io.kotest.core.spec.style.freeSpec
+import io.kotest.core.spec.style.funSpec
+import io.kotest.core.spec.style.scopes.FreeSpecContainerScope
 import io.kotest.matchers.shouldBe
+import java.io.File
 
 class Test : FreeSpec() {
     init {
-        "Test" {
-            "a" shouldBe "a"
+        "DRAW SHAPE" - {
+            "Base case" - {
+                testFactory("ex1")
+            }
+
+            "Rotation" - {
+                testFactory("ex2")
+                "Scale" - {
+                    testFactory("ex3")
+                    "Position" - {
+                        testFactory("ex4")
+                        testFactory("ex5")
+                        testFactory("ex8")
+                        testFactory("entrada1")
+                    }
+                }
+            }
+
+            "Direction" - {
+                testFactory("entrada2")
+                testFactory("ex9")
+                testFactory("ex10")
+                testFactory("ex11")
+            }
+        }
+
+        "DRAW SHAPE BASE" - {
+            testFactory("ex6")
+            testFactory("ex7")
         }
     }
+}
+
+suspend fun FreeSpecContainerScope.testFactory(testCase: String) = "Test case $testCase" {
+        val inputFilePath = "src/test/resources/casos_de_teste/input/$testCase.txt"
+        val actualOutputFilePath = "src/test/resources/tmp/$testCase.pgm"
+        val args = arrayOf(inputFilePath, actualOutputFilePath)
+
+        EP2_esqueleto.main(args)
+
+        val actualOutput = File(actualOutputFilePath).readBytes()
+        val expectedOutputFilePath = "src/test/resources/casos_de_teste/output/$testCase.pgm"
+        val expectedOutput = File(expectedOutputFilePath).readBytes()
+
+        actualOutput shouldBe expectedOutput
 }
